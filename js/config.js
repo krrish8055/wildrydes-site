7afe00409e8ba79a16714d0515d0969821e29be8
Ubuntu <ubuntu@ip-172-31-40-186.ap-south-1.compute.internal>
2020-07-29 08:22:19 +0000

    modified config.js file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_Kfja62LV4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1cdvkval4p8qmjbu2dt07h79if', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.ap-south-1.amazonaws.com/prod',
     }
 };
